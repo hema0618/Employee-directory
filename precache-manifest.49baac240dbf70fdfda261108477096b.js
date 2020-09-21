b7b025701fda01b1e47b99d35fd1726d865560c4
Hema <khemalatha507@gmail.com>
2020-09-21 15:55:19 -0400

    Updates

---
+++ precache-manifest.49baac240dbf70fdfda261108477096b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f858973508b2215b1516d478eb10cf85",
+    "url": "/Employee-directory/index.html"
+  },
+  {
+    "revision": "06e17c99aef3ff226650",
+    "url": "/Employee-directory/static/css/main.e0c799e6.chunk.css"
+  },
+  {
+    "revision": "097d6def925a86e9764a",
+    "url": "/Employee-directory/static/js/2.99a58b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-directory/static/js/2.99a58b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e17c99aef3ff226650",
+    "url": "/Employee-directory/static/js/main.12dba36e.chunk.js"
+  },
+  {
+    "revision": "c6a96534fd702eeb21ed",
+    "url": "/Employee-directory/static/js/runtime-main.f2d92646.js"
+  }
+]);
